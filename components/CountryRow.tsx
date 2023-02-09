0bfc9d1d3a64f169bf68d652a9c4791423fd6e95
rathpanha <rath.panha.rp@gmail.com>
2023-02-09 23:44:59 +0700

    listing all countries

---
+++ components/CountryRow.tsx
@@ -0,0 +1,49 @@
+import { Country } from "@/types/Country";
+import Image from "next/image";
+
+const CountryRow = ({ country }: { country: Country }) => {
+  const nameNativeRow = [];
+  for(const key in country.name.nativeName) {
+    nameNativeRow.push(<li key={key}>{country.name.nativeName[key].official}</li>);
+  }
+
+  const nameAlternative = country.altSpellings.map((name, inx) => {
+    return <li key={inx}>{name}</li>;
+  });
+
+  const callingCodes = country.idd.suffixes.map((code, inx) => {
+    return <li key={inx}>{country.idd.root}{code}</li>;
+  });
+
+  return (
+    <tr>
+      <td><Image unoptimized={true} src={country.flags.png} alt={country.name.official} width={100} height={72}/></td>
+      <td>{country.name.official}</td>
+      <td>
+        <ul>
+          {nameNativeRow}
+        </ul>
+      </td>
+      <td>
+        <ul>
+          {nameAlternative}
+        </ul>
+      </td>
+      <td>{country.cca2}</td>
+      <td>{country.cca3}</td>
+      <td> 
+        {
+          country.idd.suffixes.length > 1 ?
+            <ul>
+              {callingCodes}
+            </ul> : (
+              country.idd.suffixes.length > 0 ? 
+                `${country.idd.root}${country.idd.suffixes[0]}` : "Unknown"
+            )
+        }
+      </td>
+    </tr>
+  );
+}
+
+export default CountryRow;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,21 @@
-import '@/styles/globals.scss'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.scss';
+import { Poppins } from "@next/font/google";
+import type { AppProps } from 'next/app';
+
+const poppins = Poppins({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${poppins.style.fontFamily};
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  )
 }
