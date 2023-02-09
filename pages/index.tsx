93ee78100a84bb1b1a50bc098fc0f07e3a08a09d
rathpanha <rath.panha.rp@gmail.com>
2023-02-10 00:51:14 +0700

    search by country name

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,44 @@
 import CountryRow from '@/components/CountryRow';
 import styles from '@/styles/Home.module.scss';
 import { Country } from '@/types/Country';
+import Fuse from 'fuse.js';
 import { GetServerSideProps } from 'next';
 import Head from 'next/head';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { useRef, useState } from 'react';
 
 export default function Home({ countries }: { countries: Country[] }) {
+  const inputRef = useRef<HTMLInputElement>(null);
+  let searchTimeOut: NodeJS.Timeout;
+  const router = useRouter();
+  const { name } = router.query;
+  
+  // Set up Fuse instance for fuzzy search
+  let fuse = new Fuse(countries, {
+    keys: ["name.official", "name.common"]
+  });
+  const [ countriesFiltered, setCountriesFiltered ] = useState(!name ? countries : fuse.search(String(name)).map(result => result.item));
+
+  const onSearch = () => {
+    // Delay 1sec before searching
+    clearTimeout(searchTimeOut);
+    searchTimeOut = setTimeout (() => {
+      if(inputRef.current) {
+        console.log("search");
+        let searchName = inputRef.current.value.trim();
+        router.replace(searchName ? `/?name=${searchName}` : "/");
+
+        if(searchName) {
+          const results = fuse.search(searchName).map(result => result.item);
+          setCountriesFiltered(results);
+        } else {
+          setCountriesFiltered(countries);
+        }
+      }
+    }, 500);
+  }
+
   return (
     <>
       <Head>
@@ -13,8 +47,17 @@ export default function Home({ countries }: { countries: Country[] }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
       <main className={styles.main}>
-        <h1>Countries Catalog</h1>
+        <h1><Link href={"/"}>Countries Catalog</Link></h1>
+
+        <input 
+          ref={inputRef}
+          defaultValue={name}
+          placeholder="Search by country name..."
+          autoComplete="off"
+          onKeyDown={onSearch}
+        />
 
         <table>
           <thead>
@@ -31,7 +74,7 @@ export default function Home({ countries }: { countries: Country[] }) {
 
           <tbody>
             {
-              countries.map((country, inx) => {
+              countriesFiltered.map((country, inx) => {
                 return <CountryRow key={inx} country={country}/>;
               })
             }
